5b77b24d79524a1339e48fc607c9d00eb81573fa
Alexandre Silva <contato@silvalexandre.com>
2017-11-10 15:04:35 -0200

    Add env variables; Update gruntfile; Add nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,125 +1,102 @@
 module.exports = grunt => {
-	require('load-grunt-tasks')(grunt);
-	grunt.initConfig({
-		watch: {
-			css: {
-				files: ['src/css/*.scss'],
-				tasks: ['newer:postcss'],
-				options: {
-					spawn: false,
-					livereload: true
-				}
-			},
-			js: {
-				files: ['src/js/*.js'],
-				tasks: ['babel'],
-				options: {
-					spawn: false,
-					livereload: true
-				}
-			},
-			imgs: {
-				files: ['src/images/*.{png,jpg,gif}'],
-				tasks: ['imagemin'],
-				options: {
-					spawn: false
-				}
-			},
-			pug: {
-				files: ['src/view/*.pug'],
-				tasks: ['pug'],
-				options: {
-					spawn: false,
-					livereload: true
-				}
-			}
-		},
-		postcss: {
-			options: {
-				map: {
-					inline: false,
-					dist: 'dist/css/map/'
-				},
-				parser: require('postcss-scss'),
-				processors: [
-					require('precss')(),
-					require('postcss-cssnext')({
-						warnForDuplicates: false
-					}),
-					require('cssnano')(),
-					require('lost')(),
-					require('postcss-strip-inline-comments')()
-				],
-			},
-			dist: {
-				files: [{
-					expand: true,
-					cwd: 'src/css/',
-					src: ['*.scss'],
-					dest: 'dist/css',
-					ext: '.css',
-				}],
-			}
-		},
-		pug: {
-			compile: {
-				files: [{
-					expand: true,
-					cwd: 'src/view',
-					src: ['*.pug'],
-					dest: 'dist/',
-					ext: '.html',
-				}],
-			}
-		},
-		babel: {
-			options: {
-				sourceMap: true,
-				presets: ['es2015'],
-				comments: false,
-				sourceMaps: true,
-				minified: true,
-			},
-			dist: {
-				files: [{
-					expand: true,
-					cwd: 'src/js',
-					src: '*.js',
-					dest: 'dist/js',
-					ext: '.js',
-				}]
-			}
-		},
-		imagemin: {
-			dynamic: {
-				files: [{
-					expand: true,
-					cwd: 'src/images/',
-					src: ['**/*.{png,jpg,gif}'],
-					dest: 'dist/images/'
-				}]
-			}
-		},
-		copy: {
-			fonts: {
-				files: [{
-					expand: true,
-					cwd: 'src/fonts',
-					src: ['**/*'],
-					dest: 'dist/fonts'
-				}]
-			},
-			main: {
-				files: [{
-					expand: true,
-					cwd: 'src/main',
-					src: ['*.*'],
-					dest: 'dist'
-				}],
-			},
-		},
-	});
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['imagemin', 'pug', 'babel', 'postcss', 'copy']);
-	grunt.registerTask('init', ['imagemin', 'pug', 'babel', 'postcss', 'copy']);
-};
+  require('load-grunt-tasks')(grunt)
+  grunt.initConfig({
+    watch: {
+      css: {
+        files: ['src/css/*.scss'],
+        tasks: ['newer:postcss'],
+        options: {
+          spawn: false,
+          livereload: 8081
+        }
+      },
+      imgs: {
+        files: ['src/images/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false
+        }
+      },
+      pug: {
+        files: ['views/*.pug'],
+        options: {
+          spawn: false,
+          livereload: 8081
+        }
+      }
+    },
+    postcss: {
+      options: {
+        map: {
+          inline: false,
+          dist: 'dist/css/map/'
+        },
+        parser: require('postcss-scss'),
+        processors: [
+          require('precss')(),
+          require('postcss-cssnext')({
+            warnForDuplicates: false
+          }),
+          require('cssnano')(),
+          require('lost')(),
+          require('postcss-strip-inline-comments')()
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/css/',
+          src: ['*.scss'],
+          dest: 'dist/css',
+          ext: '.css'
+        }]
+      }
+    },
+    pug: {
+      compile: {
+        options: {
+          data: function (dest, src) {
+            return require('./config/default.json')
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/view',
+          src: ['*.pug'],
+          dest: 'dist/',
+          ext: '.html'
+        }]
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'src/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/images/'
+        }]
+      }
+    },
+    copy: {
+      fonts: {
+        files: [{
+          expand: true,
+          cwd: 'src/fonts',
+          src: ['**/*'],
+          dest: 'dist/fonts'
+        }]
+      },
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/main',
+          src: ['*.*'],
+          dest: 'dist'
+        }]
+      }
+    }
+  })
+  grunt.registerTask('default', ['watch'])
+  grunt.registerTask('deploy', ['imagemin', 'pug', 'postcss', 'copy'])
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const config = require('config')
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -11,6 +12,9 @@ app.get('/', (req, res) => {
 app.use(express.static('./dist'))
 app.use('/node', express.static(path.join(__dirname, '/node_modules')))
 app.set('view engine', 'pug')
+
+app.locals.env = process.env.NODE_ENV
+app.locals.site = config.get('site')
 
 app.listen((process.env.PORT || 8080), () => {
   console.log(`Server started at port ${(process.env.port || 8080)}`)
